582555007f72029bcffe5ddadd3b0e1c632d1213
Ranidhumma <ranidhumma8@gmail.com>
2022-07-03 23:25:38 +0530

    [Rani] Add. simple welcome message test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+it("should render message in given component", () => {
+
+  const component = render(<App />);
+  const value = component.getByTestId("welcome");
+  expect(value).toHaveTextContent("Welcome to to-do list")
+
+}
+)
+
+
+
